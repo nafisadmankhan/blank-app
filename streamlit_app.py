978079881ca064504d027a12f866ad921f374c9c
Nafi Sadman Khan <nafisadmankhan@gmail.com>
2024-07-12 15:04:39 +0600

    Updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,6 @@ animal = st.text_input('Type an animal')
 if st.button('Check availability'):
     have_it = animal.lower() in animal_shelter
     'We have that animal!' if have_it else 'We don\'t have that animal.'
+
+if st.button('Generate Question'):
+    "Here is the question"
