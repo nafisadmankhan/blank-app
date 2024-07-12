36478044397b1579d7b50b7ca0c8aff9a0b8a9f4
Nafi Sadman Khan <nafisadmankhan@gmail.com>
2024-07-12 15:02:53 +0600

    Updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+import random
 
 st.title("🎈 My new Random Number Generator app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+# Lesson 11
+random_number = random.randint(1,4)
+
+animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
+
+animal = st.text_input('Type an animal')
+
+if st.button('Check availability'):
+    have_it = animal.lower() in animal_shelter
+    'We have that animal!' if have_it else 'We don\'t have that animal.'
