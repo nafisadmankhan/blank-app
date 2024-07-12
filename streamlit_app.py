cdcfa82fb256238d95ebd83e6064d99b601b7831
Nafi Sadman Khan <nafisadmankhan@gmail.com>
2024-07-12 15:12:42 +0600

    Updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,20 @@ if st.button('Check availability'):
     'We have that animal!' if have_it else 'We don\'t have that animal.'
 
 if st.button('Generate Question'):
-    st.write("Lesson 11", random_number)
+    st.write("Lesson 01 - Question Number: ", random_number)
+    st.write("Lesson 02 - Question Number: ", random_number)
+    st.write("Lesson 03 - Question Number: ", random_number)
+    st.write("Lesson 04 - Question Number: ", random_number)
+    st.write("Lesson 05 - Question Number: ", random_number)
+    st.write("Lesson 06 - Question Number: ", random_number)
+    st.write("Lesson 07 - Question Number: ", random_number)
+    st.write("Lesson 08 - Question Number: ", random_number)
+    st.write("Lesson 09 - Question Number: ", random_number)
+    st.write("Lesson 10 - Question Number: ", random_number)
+    st.write("Lesson 11 - Question Number: ", random.randint(1,4))
+    st.write("Lesson 12 - Question Number: ", random_number)
+    st.write("Lesson 13 - Question Number: ", random_number)
+    st.write("Lesson 14 - Question Number: ", random_number)
+    st.write("Lesson 15 - Question Number: ", random_number)
+    st.write("Lesson 16 - Question Number: ", random_number)
+    st.write("Lesson 17 - Question Number: ", random_number)
